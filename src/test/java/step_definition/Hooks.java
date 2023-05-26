4aab5e10ed219dc4b1ca7a22f70d51deb20781eb
[Prathiba] <prathiba.sankararaj@gmail.com>
2023-05-26 17:01:32 +1000

    Second attempt

--- src/test/java/step_definition/Hooks.java
+++ src/test/java/step_definition/Hooks.java
@@ -10,23 +10,23 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Hooks {
-public static WebDriver driver;
-	
-	@Before 
-	public void Setup()
-	{
+	public static WebDriver driver;
+
+	@Before
+	public void Setup() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
 		driver.manage().window().maximize();
 	}
-	@After 
-	public void teardown()
-	{
-		//System.out.println("Scenario status ======>"+scenario.getSteps());
-		 //byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-         //scenario.attach(screenshot, "image/png",scenario.getName());  
-                   
+
+	@After
+	public void teardown() {
+		// System.out.println("Scenario status ======>"+scenario.getSteps());
+		// byte[] screenshot = ((TakesScreenshot)
+		// driver).getScreenshotAs(OutputType.BYTES);
+		// scenario.attach(screenshot, "image/png",scenario.getName());
+
 		driver.quit();
 	}
 
